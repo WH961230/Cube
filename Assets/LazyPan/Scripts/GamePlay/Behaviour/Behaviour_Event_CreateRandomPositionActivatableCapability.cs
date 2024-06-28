6dd61a18345b1709c4f4748940a03d6e9e062ab6
Evoreek <wh_961230@163.com>
2024-06-28 09:13:43 +0800

    可激活物体创建

---
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Event_CreateRandomPositionActivatableCapability.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LazyPan {
+    public class Behaviour_Event_CreateRandomPositionActivatableCapability : Behaviour {
+        public Behaviour_Event_CreateRandomPositionActivatableCapability(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
+	        CreateActivatableCapability();
+        }
+
+		/*获取随机位置*/
+		private LocationInformationData GetRandomPosition() {
+			LocationInformationSetting setting = Loader.LoadLocationInfSetting(entity.ObjConfig.SetUpLocationInformationSign);
+			List<LocationInformationData> datas = setting.locationInformationDatas;
+			return datas[Random.Range(0, datas.Count)];
+		}
+
+		/*创建可激活物体*/
+		private void CreateActivatableCapability() {
+			LocationInformationData data = GetRandomPosition();
+			Entity instanceEntity = Obj.Instance.LoadEntity("Obj_Activable_ActivatableCapability");
+			instanceEntity.SetBeginLocationInfo(data);
+		}
+
+        public override void Clear() {
+            base.Clear();
+        }
+    }
+}
---
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Event_CreateRandomPositionRobot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+namespace LazyPan {
+    public class Behaviour_Event_CreateRandomPositionRobot : Behaviour {
+        public Behaviour_Event_CreateRandomPositionRobot(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
+        }
+
+		/*获取随机位置*/
+		private void GetRandomPosition() {
+		}
+
+		/*创建机器人*/
+		private void CreateRobot() {
+		}
+
+
+
+        public override void Clear() {
+            base.Clear();
+        }
+    }
+}

48cb4d5f7fbaafc3cadddbbce376be032a6dc1a8
Evoreek <wh_961230@163.com>
2024-07-18 17:58:05 +0800

    提交

--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_Ring.cs
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_Ring.cs
@@ -1,15 +1,131 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace LazyPan {
     public class Behaviour_Auto_Ring : Behaviour {
+        private Transform _foot;//冲锋枪
+        private Transform _ballFoot;//子弹根节点
+        private Transform _towerFoot;//塔
+        private FloatData _fireDamage;//射击伤害
+        private Entity _targetInRangeRobotEntity;//目标范围内机器人实体
+        private List<GameObject> _balls = new List<GameObject>();
+
+        private FloatData _waveSpeed;
+        private IntData _surroundCount;
+        private GameObject _ball1;
+        private GameObject _ball2;
+        private GameObject _ball3;
+        private float waveDeploy;
+        private int surroundCount;
+        private bool isPlay;
+
         public Behaviour_Auto_Ring(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
+            //根源
+            _foot = Cond.Instance.Get<Transform>(entity, LabelStr.FOOT);
+            //子弹根节点
+            _ballFoot = Cond.Instance.Get<Transform>(entity, LabelStr.Assemble(LabelStr.BALL, LabelStr.FOOT));
+            //获取塔 跟随塔的位置
+            EntityRegister.TryGetRandEntityByType("Tower", out Entity _tower);
+            _towerFoot = Cond.Instance.Get<Transform>(_tower, LabelStr.FOOT);
+            //获取伤害
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.DAMAGE), out _fireDamage);
+            //获取环绕速度
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.SURROUND, LabelStr.SPEED), out _waveSpeed);
+            //获取环绕数量
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.SURROUND, LabelStr.COUNT), out _surroundCount);
+            //更新
+            Game.instance.OnLateUpdateEvent.AddListener(OnLateUpdate);
+            Game.instance.OnUpdateEvent.AddListener(OnUpdate);
+            
+            _balls.Clear();
         }
 
+        private bool IsActive() {
+            bool active = entity.Prefab.activeSelf;
+            //RefreshBall(active);
+            return active;
+        }
 
+        private void OnLateUpdate() {
+            if (!IsActive()) {
+                return;
+            }
+
+            SetToTowerPoint();
+        }
+
+        private void SetToTowerPoint() {
+            if (_foot != null && _towerFoot != null) {
+                _foot.position = _towerFoot.position;
+            }
+        }
+
+        private void OnUpdate() {
+            if (!IsActive()) {
+                return;
+            }
+            RingWave();
+        }
+
+        private void RingWave() {
+            if (_ballFoot != null) {
+                if (isPlay) {
+                    _ballFoot.localScale *= _waveSpeed.Float;
+                    if (_ballFoot.localScale == Vector3.one * 5) {
+                        Color c = _ballFoot.GetComponent<Image>().color;
+                        c.a = 0;
+                        _ballFoot.GetComponent<Image>().color = c;
+                        isPlay = false;
+                        waveDeploy = 3;
+                    }
+                } else {
+                    if (waveDeploy > 0) {
+                        waveDeploy -= Time.deltaTime;
+                    } else {
+                        isPlay = true;
+                    }
+                }
+            }
+        }
+
+        private void RefreshBall(bool active) {
+            if (active) {
+                _ballFoot.gameObject.SetActive(true);
+                if (surroundCount != _surroundCount.Int) {
+                    int count = _surroundCount.Int;
+
+                    while (count > 0) {
+                        GameObject tmpBall = Cond.Instance.Get<GameObject>(entity, LabelStr.Assemble(LabelStr.BALL, count.ToString()));
+                        tmpBall.SetActive(true);
+                        
+                        Comp comp = tmpBall.GetComponent<Comp>();
+                        comp.OnTriggerEnterEvent.RemoveAllListeners();
+                        comp.OnTriggerEnterEvent.AddListener(OnTriggerEnterEvent);
+                        
+                        count--;
+                    }
+
+                    surroundCount = _surroundCount.Int;
+                }
+            } else {
+                _ballFoot.gameObject.SetActive(false);
+            }
+        }
+
+        private void OnTriggerEnterEvent(Collider collider) {
+            if (EntityRegister.TryGetEntityByBodyPrefabID(collider.GetInstanceID(), out Entity bodyEntity)) {
+                if (bodyEntity.ObjConfig.Type == "Robot") {
+                    MessageRegister.Instance.Dis(MessageCode.MsgDamageRobot, bodyEntity.ID, _fireDamage.Float);
+                }
+            }
+        }
 
         public override void Clear() {
             base.Clear();
+            Game.instance.OnUpdateEvent.RemoveListener(OnUpdate);
+            Game.instance.OnLateUpdateEvent.RemoveListener(OnLateUpdate);
         }
     }
 }
--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_Strafesatellite.cs
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_Strafesatellite.cs
@@ -1,15 +1,160 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 
 namespace LazyPan {
     public class Behaviour_Auto_Strafesatellite : Behaviour {
+        private Transform _foot;//冲锋枪
+        private Transform _body;//身体
+        private Transform _muzzle;//枪口
+        private Transform _bulletFoot;//子弹根节点
+        private Transform _towerFoot;//塔
+        private FloatData _fireRateInterval;//射击速率 射击时间间隔
+        private FloatData _fireDamage;//射击伤害
+        private FloatData _fireRange;//射击范围
+        private Entity _targetInRangeRobotEntity;//目标范围内机器人实体
+        private List<GameObject> _bullets = new List<GameObject>();
+
+        private float fireRateIntervalDeploy;
+        private GameObject bulletTemplate;
+        private GameObject _fireRangeImgGo;//范围图片
         public Behaviour_Auto_Strafesatellite(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
+            //冲锋枪根源
+            _foot = Cond.Instance.Get<Transform>(entity, LabelStr.FOOT);
+            //子弹根节点
+            _bulletFoot = Cond.Instance.Get<Transform>(entity, LabelStr.Assemble(LabelStr.BULLET, LabelStr.FOOT));
+            //子弹模板
+            bulletTemplate = Cond.Instance.Get<GameObject>(entity, LabelStr.BULLET);
+            //身体
+            _body = Cond.Instance.Get<Transform>(entity, LabelStr.BODY);
+            //枪口
+            _muzzle = Cond.Instance.Get<Transform>(entity, LabelStr.MUZZLE);
+            //获取塔 跟随塔的位置
+            EntityRegister.TryGetRandEntityByType("Tower", out Entity _tower);
+            _towerFoot = Cond.Instance.Get<Transform>(_tower, LabelStr.FOOT);
+            //获取射击速率
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RATE, LabelStr.INTERVAL),
+                out _fireRateInterval);
+            //获取射击伤害
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.DAMAGE),
+                out _fireDamage);
+            //获取射击范围
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RANGE),
+                out _fireRange);
+            _fireRangeImgGo = Cond.Instance.Get<GameObject>(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RANGE));
+            //更新
+            Game.instance.OnLateUpdateEvent.AddListener(OnLateUpdate);
+            Game.instance.OnUpdateEvent.AddListener(OnUpdate);
+            
+            fireRateIntervalDeploy = _fireRateInterval.Float;
+            _bullets.Clear();
         }
 
+        private bool IsActive() {
+            bool active = entity.Prefab.activeSelf;
+            _fireRangeImgGo.SetActive(active);
+            return active;
+        }
 
+        private void OnLateUpdate() {
+            if (!IsActive()) {
+                return;
+            }
+
+            SetToTowerPoint();
+        }
+
+        private void SetToTowerPoint() {
+            if (_foot != null && _towerFoot != null) {
+                _foot.position = _towerFoot.position;
+            }
+        }
+
+        private void OnUpdate() {
+            if (!IsActive()) {
+                return;
+            }
+            RoundShoot();
+        }
+
+        private void RoundShoot() {
+            if (fireRateIntervalDeploy > 0) {
+                fireRateIntervalDeploy -= Time.deltaTime;
+            } else {
+                fireRateIntervalDeploy = _fireRateInterval.Float;
+
+                //parent round child shot
+                GameObject instanceBullet = FireParticleSystemBullet();
+                _bullets.Add(instanceBullet);
+                Comp comp = instanceBullet.GetComponent<Comp>();
+                comp.OnParticleCollisionEvent.RemoveAllListeners();
+                comp.OnParticleCollisionEvent.AddListener(OnParticleCollisionEvent);
+            }
+        }
+
+        private GameObject FireParticleSystemBullet() {
+            Transform targetRobot = Cond.Instance.Get<Transform>(_targetInRangeRobotEntity, LabelStr.BODY);
+
+            //创建子弹
+            GameObject bulletGameObject =
+                Object.Instantiate(bulletTemplate, _muzzle.position, Quaternion.identity, _bulletFoot);
+            bulletGameObject.SetActive(true);
+
+            ParticleSystem bulletInstance = bulletGameObject.GetComponent<ParticleSystem>();
+
+            //计算预瞄敌人
+            NavMeshAgent targetRobotNavMeshAgent = Cond.Instance.Get<NavMeshAgent>(_targetInRangeRobotEntity, LabelStr.NAVMESHAGENT);
+            if (ComputeDirection(targetRobot.position, _foot.position, targetRobotNavMeshAgent.velocity, bulletInstance.startSpeed, out Vector3 result)) {
+                result.y = 0;
+                _body.forward = result;
+            } else {
+                _body.forward = (targetRobot.position - _foot.position).normalized;
+            }
+
+            bulletGameObject.transform.position = _muzzle.position;
+            bulletGameObject.transform.forward = _body.forward;
+            return bulletGameObject;
+        }
+
+        private void OnParticleCollisionEvent(GameObject arg0, GameObject fxGo) {
+            if (EntityRegister.TryGetEntityByBodyPrefabID(arg0.GetInstanceID(), out Entity bodyEntity)) {
+                if (bodyEntity.ObjConfig.Type == "Robot") {
+                    MessageRegister.Instance.Dis(MessageCode.MsgDamageRobot, bodyEntity.ID, _fireDamage.Float);
+                    fxGo.SetActive(false);
+                }
+            }
+        }
+
+        #region Math
+
+        private bool ComputeDirection(Vector3 targetDir, Vector3 bulletStartPoint, Vector3 vA, float speed, out Vector3 result) {
+            var aTob = bulletStartPoint - targetDir;
+            var dc = aTob.magnitude;
+            var alpha = Vector3.Angle(aTob, vA) * Mathf.Deg2Rad;
+            var sA = vA.magnitude;
+            var r = sA / speed;
+            if (MyMathUtil.ComputeRoot((1 - r * r), 2 * dc * r * Mathf.Cos(alpha), -dc * dc, out float root1, out float root2) == 0) {
+                result = default;
+                return false;
+            }
+
+            var dA = Mathf.Max(root1, root2);
+            var t = dA / speed;
+            var c = targetDir + t * vA;
+            result = (c - bulletStartPoint).normalized;
+            return true;
+        }
+
+        #endregion
 
         public override void Clear() {
             base.Clear();
+            Game.instance.OnUpdateEvent.RemoveListener(OnUpdate);
+            Game.instance.OnLateUpdateEvent.RemoveListener(OnLateUpdate);
+            foreach (var bullet in _bullets) {
+                GameObject.Destroy(bullet);
+            }
         }
     }
 }

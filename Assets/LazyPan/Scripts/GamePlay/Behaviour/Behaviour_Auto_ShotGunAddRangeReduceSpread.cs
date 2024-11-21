9cb5af44bd2702c8500a06ae957d67f6866ddfd0
Evoreek <wh_961230@163.com>
2024-11-21 18:19:38 +0800

    霰弹枪提交

--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_ShotGunAddRangeReduceSpread.cs
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_ShotGunAddRangeReduceSpread.cs
@@ -4,6 +4,12 @@
 namespace LazyPan {
     public class Behaviour_Auto_ShotGunAddRangeReduceSpread : Behaviour {
         public Behaviour_Auto_ShotGunAddRangeReduceSpread(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
+            //增加射击间隔
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RATE, LabelStr.INTERVAL), out FloatData _fireRateInterval);
+            _fireRateInterval.Float *= 2f;
+            //增加射程
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RANGE), out FloatData _fireRange);
+            _fireRange.Float *= 2f;
         }
 
         public override void DelayedExecute() {
--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_ShotGunAddSpreadReduceRange.cs
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_ShotGunAddSpreadReduceRange.cs
@@ -4,7 +4,12 @@
 namespace LazyPan {
     public class Behaviour_Auto_ShotGunAddSpreadReduceRange : Behaviour {
         public Behaviour_Auto_ShotGunAddSpreadReduceRange(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
-            
+            //减少射击间隔
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RATE, LabelStr.INTERVAL), out FloatData _fireRateInterval);
+            _fireRateInterval.Float /= 2f;
+            //减少射程
+            Cond.Instance.GetData(entity, LabelStr.Assemble(LabelStr.FIRE, LabelStr.RANGE), out FloatData _fireRange);
+            _fireRange.Float /= 2f;
         }
 
         public override void DelayedExecute() {
